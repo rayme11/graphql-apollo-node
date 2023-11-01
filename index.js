7a2ccc21b43b31ed2b73e73c35c8a599fb38a181
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-11-01 17:24:54 -0500

    Chapter 4 final commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server');
 const SessionAPI = require('./datasources/sessions');
+const SpeakerAPI = require('./datasources/speakers');
 
-const typeDefs = require('./schema');
+const typeDefs = require('./schema.js');
 
 const resolvers = require('./resolvers.js');
 
 const dataSources = () => ({
   sessionAPI: new SessionAPI(),
+  speakerAPI: new SpeakerAPI(),
 });
 const server = new ApolloServer({
   typeDefs,
